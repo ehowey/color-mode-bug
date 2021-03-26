42e902888201778c25099706a3f2e1638c782739
ehowey <ehowey@gmail.com>
2021-03-25 21:07:21 -0600

    Add image example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
 /** @jsx jsx */
 import { jsx, Themed, useColorMode } from "theme-ui"
+import { graphql, useStaticQuery } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 import { Layout } from "gatsby-theme-catalyst-core"
 
 const HomePage = () => {
+  const data = useStaticQuery(graphql`
+    {
+      image: file(relativePath: { eq: "catalyst-site-icon.png" }) {
+        childImageSharp {
+          gatsbyImageData(width: 500, layout: CONSTRAINED)
+        }
+      }
+    }
+  `)
   const [mode] = useColorMode()
-
   const isDark = mode === "dark"
 
   return (
@@ -13,6 +23,15 @@ const HomePage = () => {
       <Themed.p sx={{ color: isDark ? "tomato" : "cornflowerblue" }}>
         Text is blue in light mode, text is red in dark mode.
       </Themed.p>
+      <GatsbyImage
+        image={data.image.childImageSharp.gatsbyImageData}
+        sx={{
+          width: "300px",
+          filter: isDark ? "invert(1)" : "none",
+        }}
+        alt="Gatsby Theme Catalyst"
+        imgStyle={{ objectFit: "contain" }}
+      />
     </Layout>
   )
 }
